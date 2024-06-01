cedf77d188c24818e3d1d1aec4f753ce0e1e1c12
Bobby-gh <koomsonrobertgh@gmail.com>
2024-06-01 22:22:49 +0000

    fixing get routes

--- src/app/components/charts.jsx
+++ src/app/components/charts.jsx
@@ -33,7 +33,7 @@ export function OrganizationPerformanceDashboard() {
         </span>
       </div>
       <ResponsiveContainer height={355}>
-        <BarChart data={data}>
+        <BarChart>
           <Legend iconType="circle" iconSize="6" align="left" />
           <Tooltip />
           <YAxis />
