03f191be6e8f60dfa386f7d66ecab6c3a39ae588
Bobby-gh <koomsonrobertgh@gmail.com>
2024-06-01 15:26:05 +0000

    fixing get routes

--- src/app/components/infocards.jsx
+++ src/app/components/infocards.jsx
@@ -5,12 +5,13 @@ import { useGoalCountRouteData } from "../api/databook/route-data";
 
 export function InformationalSummary() {
   const { goalCount, error } = useGoalCountRouteData();
+  const { achieved, notAchieved, partiallyAchieved } = goalCount;
 
   if (error) {
     return <div>Error: {error.message}</div>;
   }
 
-console.log(goalCount)
+console.log(achieved)
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 ">
       <div className="card shadow-lg bg-gray-200 rounded-lg p-4 border border-2 border-slate-400">
