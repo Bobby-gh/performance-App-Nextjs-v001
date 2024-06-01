6871b9eb4d67a412affe80e6d62c37b7dfa33dde
Bobby-gh <koomsonrobertgh@gmail.com>
2024-06-01 01:21:53 +0000

    texting counter route

--- src/app/components/infocards.jsx
+++ src/app/components/infocards.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaThumbsDown, FaTrophy, FaThumbsUp } from "react-icons/fa";
 import { GetGoalCountRouteData } from "../api/databook/route-data";
 
