868f02d946aedfcea8c904982eaa67994d77f5da
Bobby-gh <koomsonrobertgh@gmail.com>
2024-06-01 14:55:58 +0000

    fixing get routes

--- src/app/components/infocards.jsx
+++ src/app/components/infocards.jsx
@@ -10,7 +10,7 @@ export function InformationalSummary() {
     return <div>Error: {error.message}</div>;
   }
 
-console.log(goalCount.goalRatings.achieved)
+console.log(goalCount.achieved)
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 ">
       <div className="card shadow-lg bg-gray-200 rounded-lg p-4 border border-2 border-slate-400">
