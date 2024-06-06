bd23959bd1d60b8f080d1fc914ea4d187a34a3ee
Bobby-gh <koomsonrobertgh@gmail.com>
2024-06-06 23:55:29 +0000

    updating charts

--- src/app/components/charts.jsx
+++ src/app/components/charts.jsx
@@ -37,8 +37,8 @@ export function OrganizationPerformanceDashboard() {
           <Legend iconType="circle" iconSize="6" align="left" />
           <Tooltip />
           <YAxis />
-          <XAxis datakey="departmentName" />
-          <Bar datakey="average" fill="#2394cc"/>
+          <XAxis dataKey="departmentName" />
+          <Bar dataKey="average" fill="#2394cc" />
         </BarChart>
       </ResponsiveContainer>
       <hr className="h-px my-6 border-0 dark:bg-gray-700" />
