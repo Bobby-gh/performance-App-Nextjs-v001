a06cea1f1c13a26a9e3460d602d9f8134a5d34b9
Bobby-gh <koomsonrobertgh@gmail.com>
2024-06-11 12:44:59 +0000

    remove all lengends from chats

--- src/app/components/infocards.jsx
+++ src/app/components/infocards.jsx
@@ -1,7 +1,6 @@
-"use client"
+"use client";
 import { FaThumbsDown, FaTrophy, FaThumbsUp } from "react-icons/fa";
 import { useGoalCountRouteData } from "../api/databook/route-data";
-
 
 export function InformationalSummary() {
   const { goalCount, error } = useGoalCountRouteData();
@@ -10,7 +9,6 @@ export function InformationalSummary() {
     return <div>Error: {error.message}</div>;
   }
 
-  
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 ">
       <div className="card shadow-lg bg-gray-200 rounded-lg p-4 border border-2 border-slate-400">
@@ -20,10 +18,10 @@ export function InformationalSummary() {
           </span>
           <span className="flex flex-col items-end">
             <h3 className="text-green-900">Achieved Goals</h3>
-          <h3 className="font-bold text-xl">{achieved}</h3>
+            <h3 className="font-bold text-xl">{achieved}</h3>
           </span>
         </div>
-        <hr className="h-px my-6 border-0 dark:bg-gray-700"/>
+        <hr className="h-px my-6 border-0 dark:bg-gray-700" />
         <div className="flex">
           <span className="text-blue-900">+55%</span>
           <span className="ml-2">than last Month</span>
@@ -311,10 +309,10 @@ export function UserPerformanceSummary() {
   );
 }
 
-
-export function DataDateAccess(){
-  return(
-    <div className="flex flex-row-reverse pt-8">
+export function DataDateAccess() {
+  return (
+    <div className=" mt-8">
+      <div className="flex flex-row-reverse">
         <div className="p-3 mr -2">
           <div className="relative mb-6" data-te-input-wrapper-init>
             <input
@@ -357,5 +355,6 @@ export function DataDateAccess(){
           </div>
         </div>
       </div>
-  )
+    </div>
+  );
 }
