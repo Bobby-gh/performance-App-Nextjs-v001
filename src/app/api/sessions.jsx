40681deb541d2a937cb8c041fa2f3f3143aec5d1
Bobby-gh <koomsonrobertgh@gmail.com>
2024-06-11 14:19:20 +0000

    adding lodding popup

--- src/app/api/sessions.jsx
+++ src/app/api/sessions.jsx
@@ -1,25 +1,19 @@
-'use client'
+"use client";
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
 import { useEffect, useState } from "react";
 
-
 export const LoadingPopup = () => {
-    const [isLoading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      const delay = setTimeout(() => {
-        setLoading(false); 
-        clearTimeout(delay);
-      }, 2000);
-      return () => {
-        clearTimeout(delay);
-      };
-    }, []);
-  
-    return (
-      <Backdrop open={isLoading} style={{ zIndex: 9999, color: "#fff" }}>
-        <CircularProgress color="inherit" />
-      </Backdrop>
-    );
-  };
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  return (
+    <Backdrop open={isLoading} style={{ zIndex: 9999, color: "#fff" }}>
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  );
+};
