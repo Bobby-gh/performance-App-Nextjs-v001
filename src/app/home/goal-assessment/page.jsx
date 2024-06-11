2e5c4ed85fb90c97b0a710625b49821890cdb12f
Bobby-gh <koomsonrobertgh@gmail.com>
2024-06-11 15:30:34 +0000

    adding lodding popup

--- src/app/home/goal-assessment/page.jsx
+++ src/app/home/goal-assessment/page.jsx
@@ -1,3 +1,4 @@
+import { LoadingPopup } from "@/app/api/sessions";
 import { AccessGoal } from "@/app/components/drawers";
 import { AccessGoalTable } from "@/app/components/tables";
 import React from "react";
@@ -5,6 +6,7 @@ import React from "react";
 export default function GoalAssessment() {
   return (
     <main className="m-4">
+      <LoadingPopup/>
       <div className="mb-4 flex justify-between flex-row">
         <div className="text-xl font-bold text-blue-500">Assess New Goals</div>
         <AccessGoal/>
