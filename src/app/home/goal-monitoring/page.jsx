f170d5cae3282a223bfa6efba2d687a0d6267bed
Bobby-gh <koomsonrobertgh@gmail.com>
2024-06-11 11:09:01 +0000

    organizational chat

--- src/app/home/goal-monitoring/page.jsx
+++ src/app/home/goal-monitoring/page.jsx
@@ -9,8 +9,8 @@ export default function GoalMonitoring() {
         <div className="text-xl font-bold text-blue-500">Monitor New Goals</div>
         <div>Monitor Goals</div>
       </div>
-      <div className="flex flex-col">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+      <div className="flex flex-col ">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 card shadow-lg bg-gray-200 rounded-lg p-4 border border-2 border-slate-400">
           <div className="col-span-2">
             <ProgressLineChat />
           </div>
