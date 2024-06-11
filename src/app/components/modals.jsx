b63ef5f84d76d070a6042c69771ff2fb5cfbeb1a
Bobby-gh <koomsonrobertgh@gmail.com>
2024-06-11 14:02:37 +0000

    adding lodding popup

--- src/app/components/modals.jsx
+++ src/app/components/modals.jsx
@@ -1,4 +1,7 @@
+'use client'
+import { Box, CircularProgress, Modal, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
+import { FaSignOutAlt } from "react-icons/fa";
 
 export function LogOut() {
   const [open, setOpen] = React.useState(false);
@@ -72,7 +75,7 @@ export function LogOut() {
               className="flex flex row items-center p-3 m-2 bg-transparent hover:bg-blue-900 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
               type="submit"
               onClick={handleLogOut}
-              disabled={isLoading} // Disable the button while loading
+              disabled={isLoading} 
             >
               {isLoading ? (
                 <div className="flex flex-row justify-center">
