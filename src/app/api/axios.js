3669602ead4bdd848046bba69f6ba63acea45d41
Bobby-gh <knaihv@ymail.com>
2024-09-30 13:35:02 +0000

    changed routes

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://performance-app.up.railway.app/api/v1/",
+  baseURL: "http://performanceappbackendv001-production.up.railway.app//api/v1/",
 });
