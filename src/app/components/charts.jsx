1e2d96045e3403c46acfbc24e653468b07f0494b
Bobby-gh <knaihv@ymail.com>
2024-10-01 14:32:11 +0000

    working on dashboard

--- src/app/components/charts.jsx
+++ src/app/components/charts.jsx
@@ -104,7 +104,7 @@ export function GeneralPerformanceDashboard() {
         <div className="text-lg font-bold pb-8 text-black">
           General Performance
         </div>
-        <div className="flex items-center">
+        <div className="flex items-center justify-center">
           <Gauge
             value={generalPerformance}
             height={270}
