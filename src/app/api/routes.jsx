9fcd7764b3551083834cc7d4c6e1ea05947a9448
Bobby-gh <knaihv@ymail.com>
2024-10-01 14:47:34 +0000

    working on dashboard

--- src/app/api/routes.jsx
+++ src/app/api/routes.jsx
@@ -7,7 +7,7 @@ export const DEPARTMENT_GOAL_ASSESSMENT_URL ="/deparment-goal-assessments"
 export const LOGIN_URL ="/login"
 export const PERFORMANCE_MATRIX_CHART_URL = "/department-performance"
 export const GENERAL_PERFORMANCE_CHART_URL = "/department-performance/overall-average"
-export const ORGANIZATIONAL_CHART_URL = "department-performance/get-monthly-average-performance "
+export const ORGANIZATIONAL_CHART_URL = "/department-performance/get-monthly-average-performance "
 export const GOAL_COUNT_URL = "/department-performance/ratings-count"
 
 
