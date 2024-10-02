599362dbb3dce527fd98986902c57d748dfa38cc
Bobby-gh <knaihv@ymail.com>
2024-10-02 17:24:03 +0000

    working on dashboard

--- src/app/home/department/page.jsx
+++ src/app/home/department/page.jsx
@@ -10,8 +10,8 @@ export default function Department() {
       <LoadingPopup/>
       <div className="mb-4 flex justify-between flex-row">
         <div className="text-xl font-bold text-blue-500">Organizational Department</div>
+        <Departmentforms/>
       </div>
-      <AddDepartment/>
       <div className="mt-16 bg-white rounded-lg">
         <DepartmentTable />
       </div>
--- src/app/home/employees/page.jsx
+++ src/app/home/employees/page.jsx
@@ -10,8 +10,8 @@ export default function Employees() {
       <LoadingPopup/>
       <div className="mb-4 flex justify-between flex-row">
         <div className="text-xl font-bold text-blue-500">Organizational Employees</div>
+        <Userforms/>
       </div>
-      <AddUser/>
       <div className="mt-16 bg-white rounded-lg">
         <EmployeeTable /> 
       </div>
