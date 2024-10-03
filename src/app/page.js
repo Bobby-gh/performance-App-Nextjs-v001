b50afce8dd628bfe73c4eaeb30a9d58d65c56a5f
Bobby-gh <knaihv@ymail.com>
2024-10-03 13:37:32 +0000

    working on dashboard

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import { LoginForm } from "./components/forms";
 
 export default function Login() {
   return (
-    <main className="flex justify-center h-screen bg-[url('https://th.bing.com/th/id/OIP.MDYhGld3AqGzENMP1WPFPwHaFc?rs=1&pid=ImgDetMain')]">
+    <main className="flex justify-center h-screen bg-[url('https://img.freepik.com/premium-photo/close-up-business-man-hand-pointing-creative-glowing-business-chart-interface-hologram_670147-12910.jpg')]">
       <div className="card rounded-lg flex flex-wrap m-8 border border-blue-950 shadow-lg shadow-blue-950 p-8 bg-slate-300">
         <LoginForm/>
       </div>
