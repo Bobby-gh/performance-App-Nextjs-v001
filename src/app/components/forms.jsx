2697b64bae8d95b1b75eabeb364009ef2bded126
Bobby-gh <knaihv@ymail.com>
2024-11-05 11:53:13 +0000

    working on login page

--- src/app/components/forms.jsx
+++ src/app/components/forms.jsx
@@ -122,3 +122,152 @@ export function LoginForm() {
     </main>
   );
 }
+
+export function SignUpForm() {
+  const router = useRouter();
+  const [isLoading, setLoading] = useState(false);
+  const { auth, setAuth } = React.useContext(AuthContext);
+  const [userDetails, setUserDetails] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({
+          email: userDetails.email,
+          password: userDetails.password,
+        }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      if (response.request.status === 200) {
+        setAuth({
+          token: response.data.token,
+        });
+      }
+    } catch (err) {
+      alert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <main className="w-96">
+      <div>
+        <div className="flex justify-center p-8">
+          <img
+            src="https://afriquetek.com/wp-content/uploads/2023/07/afriquetek-logo-1.png"
+            alt="Paris"
+            className="w-55 h-20"
+          />
+        </div>
+        <form autoComplete="off">
+          <div className="grid grid-cols-2 gap-2">
+            <div className="flex flex-col">
+              <label>Email</label>
+              <input
+                placeholder="Type email here"
+                autoComplete="off"
+                type="email"
+                value={userDetails.email}
+                onChange={(e) =>
+                  setUserDetails((prevDetails) => ({
+                    ...prevDetails,
+                    email: e.target.value,
+                  }))
+                }
+                className="border border-blue-500 rounded-lg p-4 my-2"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label>Email</label>
+              <input
+                placeholder="Type email here"
+                autoComplete="off"
+                type="email"
+                value={userDetails.email}
+                onChange={(e) =>
+                  setUserDetails((prevDetails) => ({
+                    ...prevDetails,
+                    email: e.target.value,
+                  }))
+                }
+                className="border border-blue-500 rounded-lg p-4 my-2"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <label>Email</label>
+            <input
+              placeholder="Type email here"
+              autoComplete="off"
+              type="email"
+              value={userDetails.email}
+              onChange={(e) =>
+                setUserDetails((prevDetails) => ({
+                  ...prevDetails,
+                  email: e.target.value,
+                }))
+              }
+              className="border border-blue-500 rounded-lg p-4 my-2"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label>Password</label>
+            <input
+              placeholder="Enter password"
+              autoComplete="off"
+              type="password"
+              value={userDetails.password}
+              onChange={(e) =>
+                setUserDetails((prevDetails) => ({
+                  ...prevDetails,
+                  password: e.target.value,
+                }))
+              }
+              className="border border-blue-500 rounded-lg p-4 my-2"></input>
+          </div>
+          <div
+            className="flex justify-center p-4 text-white rounded-lg mt-8 bg-slate-500"
+            onClick={handleSubmit}>
+            <button type="submit" disabled={isLoading} className="px-16">
+              {isLoading ? (
+                <div className="flex flex-row justify-center">
+                  <p className="text-sm pr-2">Loading</p>
+                  <CircularProgress size={27} thickness={6} color="primary" />
+                </div>
+              ) : (
+                "Submit"
+              )}
+            </button>
+          </div>
+          {/* or line */}
+          <div className="flex items-center justify-center my-6">
+            <div className="flex-grow border-t border-slate-500"></div>
+            <span className="px-2 text-sm text-gray-500">or</span>
+            <div className="flex-grow border-t border-slate-500"></div>
+          </div>
+
+          {/* sign up */}
+          <div className="flex justify-center items-center space-x-1">
+            <span className="text-sm text-black">Dont have an account?</span>
+            <span className="text-[#04B1C4] text-sm">
+              <Link href="/signup" prefetch={false}>
+                Sign Up
+              </Link>
+            </span>
+          </div>
+        </form>
+        {auth.token && router.push("/home", { scroll: false })}
+      </div>
+    </main>
+  );
+}
