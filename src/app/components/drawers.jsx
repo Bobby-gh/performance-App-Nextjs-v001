505152e6d68f58bb2ed7ae6591594735147745e0
Bobby-gh <knaihv@ymail.com>
2024-11-06 12:47:40 +0000

    working on route

--- src/app/components/drawers.jsx
+++ src/app/components/drawers.jsx
@@ -32,7 +32,7 @@ export function CreateGoal() {
           goalTitle: formData.title,
           goalDescription: formData.description,
           goalDeadline: formData.endDate,
-          taskAssignedTo: formData.department,
+          taskAssignedTo: departments,
         },
         {
           headers: {
@@ -87,7 +87,7 @@ export function CreateGoal() {
             <div className="relative mb-6" data-te-input-wrapper-init>
               <select
                 id="department"
-                value={formData.department}
+                value={departments}
                 onChange={(e) => setDepartments(e.target.value)}
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 required>
