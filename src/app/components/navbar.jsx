a92b63a09a9c8ec41d0682cbdb0f374faaa35a67
Bobby-gh <knaihv@ymail.com>
2024-11-12 14:52:54 +0000

    working on goal

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { IoNotifications } from "react-icons/io5";
 import { LogOut } from "./modals";
+import { AuthContext } from "../contex/context-context";
 
 export function Navbar() {
+  const {role} = React.useContext(AuthContext)
   return (
-    <nav className="h-16 flex justify-end items-center card rounded-lg">
+    <nav className="h-16 flex justify-between items-center card rounded-lg">
+      <div className="flex flex-row items-center"><span className="text-sm font-bold">Welcome Back</span>{role}</div>
       <div className="flex items-center">
         <LogOut/>
         <IoNotifications className="ml-6"/>
