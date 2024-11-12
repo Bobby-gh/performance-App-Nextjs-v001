cfc5ae658484cb1544bde93dc0ed702a7ac2ea57
Bobby-gh <knaihv@ymail.com>
2024-11-12 16:40:07 +0000

    working on goal

--- src/app/components/drawers.jsx
+++ src/app/components/drawers.jsx
@@ -15,7 +15,7 @@ import { AuthContext } from "../contex/context-context";
 
 export function CreateGoal() {
   const { auth } = useContext(AuthContext);
-  const {departmenttable } = useDepartmentRouteData();
+  const { departmenttable } = useDepartmentRouteData();
   const [departments, setDepartments] = useState([]);
   const [open, setOpen] = React.useState(false);
   const [isLoading, setLoading] = useState(false);
@@ -27,7 +27,6 @@ export function CreateGoal() {
     department: "",
   });
 
-  
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
@@ -476,8 +475,8 @@ export function AccessGoal() {
 export function Userforms(props) {
   const { auth } = useContext(AuthContext);
   const [departments, setDepartments] = useState([]);
-  const {departmenttable } = useDepartmentRouteData();
-  const [role, setRole] = useState("")
+  const { departmenttable } = useDepartmentRouteData();
+  const [role, setRole] = useState("");
   const [isLoading, setLoading] = useState(false);
   const [userFormData, setUserFormData] = useState({
     fullName: "",
@@ -595,12 +594,12 @@ export function Userforms(props) {
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 value={role}
                 onChange={(e) => setRole(e.target.value)}
-                required
-              />
-              <option>Select ...</option>
-              <option value="generalManager">General Manager </option>
-              <option value="manager">Manager </option>
-              <option value="staff">Staff </option>
+                required>
+                <option>Select ...</option>
+                <option value="generalManager">General Manager </option>
+                <option value="manager">Manager </option>
+                <option value="staff">Staff </option>
+              </select>
               <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                 Password
               </label>
