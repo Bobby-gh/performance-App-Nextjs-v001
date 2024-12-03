2761c992ae56b3f80db410484fb1fbd05404c599
Bobby-gh <knaihv@ymail.com>
2024-12-03 21:57:20 +0000

    logout

--- src/app/components/drawers.jsx
+++ src/app/components/drawers.jsx
@@ -224,7 +224,7 @@ export function AccessGoal() {
     try {
       await axios.post(
         JSON.stringify({
-          goalId: assessData.goalId,
+          goalId: goal,
           workQuality: assessData.workQuality,
           productivity: assessData.productivity,
           communication: assessData.communication,
@@ -291,7 +291,7 @@ export function AccessGoal() {
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 required>
                 <option value="" disabled>
-                  Select a department
+                  Select a goal
                 </option>
                 {departmentgoaltable.map((goals) => (
                   <option
