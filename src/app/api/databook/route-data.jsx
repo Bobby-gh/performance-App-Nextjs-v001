537623ff5b5f3a08ac4b05acf8e6edc7cbf1d508
Bobby-gh <knaihv@ymail.com>
2024-12-05 22:18:30 +0000

    my goals

--- src/app/api/databook/route-data.jsx
+++ src/app/api/databook/route-data.jsx
@@ -41,7 +41,7 @@ export function useGoalRouteData() {
           },
           withCredentials: true,
         });
-        console.log(response.data);
+        console.log({'reponse goal': response.data});
         setDepartmenttable(response.data);
       } catch (err) {
         console.log(err);
