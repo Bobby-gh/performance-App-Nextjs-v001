71d5c40cf95181db828586561217ba8a91af87fb
Bobby-gh <knaihv@ymail.com>
2024-12-06 12:01:50 +0000

    my goals

--- src/app/home/goals/page.jsx
+++ src/app/home/goals/page.jsx
@@ -1,33 +1,30 @@
-'use client'
+"use client";
 import { useMyGoalRouteData } from "@/app/api/databook/route-data";
 import { AddUser, Goals } from "@/app/components/infocards";
 import React from "react";
 
-
-
 export default function MyGoals() {
   const { mygoal } = useMyGoalRouteData();
-  console.log({"my goals:": mygoal})
+  console.log({ "my goals:": mygoal });
 
   return (
-    <main className="mt-8 flex">
-    <div className="flex-[1]">
-      <AddUser />
-    </div>
-    <div className="flex-[1.5]">
-      <div className="grid grid-cols-2 2xl:grid-cols-3 gap-4 cursor-pointer">
-        {mygoal.map((goal, index) => (
-          <Goals
-            key={index}
-            id={goal.id}
-            goalTitle={goal.goalTitle}
-            goalDeadline={goal.goalDeadline}
-            status={goal.status}
-          />
-        ))}
+    <main className="m-4">
+      <div className="flex-[1]">
+        <AddUser />
       </div>
-    </div>
-  </main>
+      <div className="flex-[1.7]">
+        <div className="grid grid-cols-2 2xl:grid-cols-3 gap-4 cursor-pointer">
+          {mygoal.map((goal, index) => (
+            <Goals
+              key={index}
+              id={goal.id}
+              goalTitle={goal.goalTitle}
+              goalDeadline={goal.goalDeadline}
+              status={goal.status}
+            />
+          ))}
+        </div>
+      </div>
+    </main>
   );
 }
-
