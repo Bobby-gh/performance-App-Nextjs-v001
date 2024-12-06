314a6c40046718bc247d778ef833eed7b92c2190
Bobby-gh <knaihv@ymail.com>
2024-12-06 11:13:08 +0000

    my goals

--- src/app/home/goals/page.jsx
+++ src/app/home/goals/page.jsx
@@ -6,6 +6,7 @@ import React from "react";
 
 export default function MyGoals() {
   const { mygoal } = useMyGoalRouteData();
+  console.log(mygoal)
 
   return (
     <main className="mt-8 flex">
@@ -14,7 +15,7 @@ export default function MyGoals() {
     </div>
     <div className="flex-[2]">
       <div className="grid grid-cols-3 2xl:grid-cols-4 gap-4 cursor-pointer">
-        {mygoal.map((goal, index) => (
+        {/* {mygoal.map((goal, index) => (
           <Goals
             key={index}
             id={goal.id}
@@ -22,7 +23,7 @@ export default function MyGoals() {
             goalDeadline={goal.goalDeadline}
             status={goal.status}
           />
-        ))}
+        ))} */}
       </div>
     </div>
   </main>
