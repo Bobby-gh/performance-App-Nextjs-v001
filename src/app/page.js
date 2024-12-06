279e344c96e5ec09be20a5462d9a598fa3fc90ed
Bobby-gh <knaihv@ymail.com>
2024-12-06 21:20:32 +0000

    logout

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import { LoginForm } from "./components/forms";
 
 export default function Login() {
   return (
-    <main className="flex justify-center items-center h-screen bg-[url('https://www.afmssc.com/images/AFMS/Home/AFMS_Showcase/Image3.jpg#joomlaImage:/local-images/AFMS/Home/AFMS_Showcase/Image3.jpg?width=1800&height=1000')]">
+    <main className="flex justify-center items-center h-screen bg-[url('https://img.freepik.com/premium-photo/analytics-business-intelligence-concept_488220-10421.jpg')]">
       <div className="card rounded-lg flex h-[80%] flex-wrap m-8 border border-slate-300 shadow-sm p-8 bg-slate-300">
         <LoginForm/>
       </div>
