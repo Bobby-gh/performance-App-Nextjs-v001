4302ea3c8d13c349e37d987c9dae39ff878a00b3
Bobby-gh <knaihv@ymail.com>
2024-12-07 08:29:51 +0000

    logout

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import { LoginForm } from "./components/forms";
 
 export default function Login() {
   return (
-    <main className="flex justify-center items-center h-screen bg-[url('https://img.freepik.com/premium-vector/vector-illustration-business-team-analysis-monitoring-web-report-dashboard-monitor_675567-3176.jpg?w=2000')]">
+    <main className="flex justify-center items-center h-screen bg-[url('https://static.vecteezy.com/system/resources/previews/029/179/477/original/digital-marketing-team-ready-to-use-banner-vector.jpg')]">
       <div className="card rounded-lg flex h-[80%] flex-wrap m-8 border border-slate-300 shadow-sm p-8 bg-slate-300">
         <LoginForm/>
       </div>
