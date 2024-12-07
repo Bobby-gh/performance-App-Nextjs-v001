54484147b9f85b1526dc7191fa1046bf4c5f74ba
Bobby-gh <knaihv@ymail.com>
2024-12-07 09:22:45 +0000

    logout

--- src/app/verifyemail/page.jsx
+++ src/app/verifyemail/page.jsx
@@ -2,8 +2,8 @@ import { VerifyEmailForm } from "../components/forms";
 
 export default function VerifyEmail() {
   return (
-    <main className="flex justify-center items-center h-screen bg-[url('https://www.afmssc.com/images/AFMS/Home/AFMS_Showcase/Image3.jpg#joomlaImage:/local-images/AFMS/Home/AFMS_Showcase/Image3.jpg?width=1800&height=1000')]">
-      <div className="card rounded-lg flex h-[80%] flex-wrap m-8 border border-blue-950 shadow-lg shadow-blue-950 p-8 bg-slate-300">
+    <main className="flex justify-end items-center h-screen bg-[url('https://static.vecteezy.com/system/resources/previews/011/579/414/non_2x/busines-using-a-computer-to-complete-individual-income-tax-return-form-online-for-tax-payment-government-state-taxes-data-analysis-paperwork-financial-research-report-calculation-tax-return-free-photo.jpg')]">
+      <div className="card  flex h-[100%] flex-wrap  border border-slate-700 shadow-sm p-8 bg-slate-300">
         <VerifyEmailForm/>
       </div>
     </main>
