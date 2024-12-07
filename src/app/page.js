e0ea9f39b6c2526ce571ef748f932149ec97e563
Bobby-gh <knaihv@ymail.com>
2024-12-07 08:22:46 +0000

    logout

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import { LoginForm } from "./components/forms";
 
 export default function Login() {
   return (
-    <main className="flex justify-center items-center h-screen bg-[url('https://img.freepik.com/premium-photo/analytics-business-intelligence-concept_488220-10421.jpg')]">
+    <main className="flex justify-center items-center h-screen bg-[url('https://www.aihr.com/wp-content/uploads/performance-management-cover.png')]">
       <div className="card rounded-lg flex h-[80%] flex-wrap m-8 border border-slate-300 shadow-sm p-8 bg-slate-300">
         <LoginForm/>
       </div>
