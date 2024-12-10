b73d3440e94a4c805de729b08ccbb39e5b883269
Bobby-gh <knaihv@ymail.com>
2024-12-10 07:23:25 +0000

    logout

--- src/app/verifyemail/page.jsx
+++ src/app/verifyemail/page.jsx
@@ -2,8 +2,8 @@ import { VerifyEmailForm } from "../components/forms";
 
 export default function VerifyEmail() {
   return (
-    <main className="flex justify-end items-center h-screen bg-[url('https://static.vecteezy.com/system/resources/previews/011/579/414/non_2x/busines-using-a-computer-to-complete-individual-income-tax-return-form-online-for-tax-payment-government-state-taxes-data-analysis-paperwork-financial-research-report-calculation-tax-return-free-photo.jpg')]">
-      <div className="card  flex h-[100%] flex-wrap  border border-slate-700 shadow-sm p-8 bg-slate-300">
+    <main className="flex justify-center items-center h-screen bg-[url('https://thrivenextgen.com/wp-content/uploads/Acronyms2-1-scaled.webp')]">
+      <div className="card  flex h-[90%] flex-wrap rounded-lg  border border-slate-300 shadow-sm p-8 bg-white">
         <VerifyEmailForm/>
       </div>
     </main>
