cd6f795bcf8c350237da112db1a42307367d0ec6
Bobby-gh <knaihv@ymail.com>
2024-12-11 00:49:01 +0000

    logout

--- src/app/components/drawers.jsx
+++ src/app/components/drawers.jsx
@@ -28,7 +28,7 @@ export function CreateGoal() {
   const [departments, setDepartments] = useState([]);
   const [open, setOpen] = React.useState(false);
   const [isLoading, setLoading] = useState(false);
-  const [priority, setPriority] = useState("")
+  const [priority, setPriority] = useState("");
   const [formData, setFormData] = useState({
     title: "",
     description: "",
@@ -49,7 +49,7 @@ export function CreateGoal() {
           goalDeadline: formData.endDate,
           taskAssignedTo: departments,
           target: formData.target,
-          priority: priority
+          priority: priority,
         }),
         {
           headers: {
@@ -96,51 +96,57 @@ export function CreateGoal() {
       </Button>
       <Drawer anchor={"right"} open={open} onClose={handleClose}>
         <div className="flex justify-center font-bold py-5  text-black">
-          Assign a new Goal
+          Assign a New Goal
         </div>
         <hr />
         <form className="w-96">
           <div className=" px-10 py-8">
             <div className="relative mb-6" data-te-input-wrapper-init>
               {auth.refNum === "ref?1!" || auth.refNum === "ref?1!" ? (
-                <select
-                  id="department"
-                  value={departments}
-                  onChange={(e) => setDepartments(e.target.value)}
-                  className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                  required>
-                  <option value="" disabled>
-                    Select a department
-                  </option>
-                  {departmenttable.map((department) => (
-                    <option
-                      key={department.departmentId}
-                      value={department.departmentId}>
-                      {department.departmentName}
+                <>
+                  <select
+                    id="department"
+                    value={departments}
+                    onChange={(e) => setDepartments(e.target.value)}
+                    className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                    required>
+                    <option value="" disabled>
+                      Select a department
                     </option>
-                  ))}
-                </select>
+                    {departmenttable.map((department) => (
+                      <option
+                        key={department.departmentId}
+                        value={department.departmentId}>
+                        {department.departmentName}
+                      </option>
+                    ))}
+                  </select>
+                  <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                    Department
+                  </label>
+                </>
               ) : (
-                <select
-                  id="department"
-                  value={departments}
-                  onChange={(e) => setDepartments(e.target.value)}
-                  className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                  required>
-                  <option value="" disabled>
-                    Select a Staff ..
-                  </option>
-                  {employeetable.map((department) => (
-                    <option key={department.userId} value={department.userId}>
-                      {department.name}
+                <>
+                  <select
+                    id="department"
+                    value={departments}
+                    onChange={(e) => setDepartments(e.target.value)}
+                    className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                    required>
+                    <option value="" disabled>
+                      Select a Staff ..
                     </option>
-                  ))}
-                </select>
+                    {employeetable.map((department) => (
+                      <option key={department.userId} value={department.userId}>
+                        {department.name}
+                      </option>
+                    ))}
+                  </select>
+                  <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                    Staff
+                  </label>
+                </>
               )}
-
-              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-                Staff
-              </label>
             </div>
             <div className="relative mb-6" data-te-input-wrapper-init>
               <input
