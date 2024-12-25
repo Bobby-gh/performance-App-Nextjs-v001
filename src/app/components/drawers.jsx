37b9dbffe777f553fc8d54e78f63bb31e65b74d3
Bobby-gh <knaihv@ymail.com>
2024-12-25 02:27:57 +0000

    working on colors

--- src/app/components/drawers.jsx
+++ src/app/components/drawers.jsx
@@ -346,7 +346,6 @@ export function AccessGoal() {
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 required>
                 <option value="" disabled>
-                  l
                 </option>
                 {departmentgoal.map((goals) => (
                   <option key={goals._id} value={goals._id}>
