6b2260c8ad01c23a43eab1ea09de894d5aadb137
Bobby-gh <knaihv@ymail.com>
2024-12-25 02:16:09 +0000

    working on colors

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -9,7 +9,7 @@ export function Navbar() {
   console.log({"navbar auth": auth})
   return (
     <nav className="h-16 flex justify-between items-center card rounded-lg p-4">
-      <div className="flex flex-row items-center"><span className="text-lg font-bold">Welcome Back</span ><span className="ml-2 text-lg font-bold text-slate-400">{auth.name}</span></div>
+      <div className="flex flex-row items-center"><span className="text-lg font-bold">Welcome Back</span ><span className="ml-2 text-lg font-bold text-blue-900">{auth.name}</span></div>
       <div className="flex items-center">
         <LogOut/>
         <IoNotifications className="ml-6"/>
