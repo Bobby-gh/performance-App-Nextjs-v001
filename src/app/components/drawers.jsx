8b6d2bbf1a3e5dbfedd200edbdaf14c90305dc0c
Bobby-gh <knaihv@ymail.com>
2024-12-25 02:24:39 +0000

    working on colors

--- src/app/components/drawers.jsx
+++ src/app/components/drawers.jsx
@@ -111,7 +111,6 @@ export function CreateGoal() {
                     className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                     required>
                     <option value="" disabled>
-                      Select a department
                     </option>
                     {departmenttable.map((department) => (
                       <option
@@ -183,7 +182,7 @@ export function CreateGoal() {
                 value={priority}
                 onChange={(e) => setPriority(e.target.value)}
                 required>
-                <option>Select ...</option>
+                <option></option>
                 <option value="low">Low </option>
                 <option value="medium">Medium </option>
                 <option value="high">High </option>
@@ -347,7 +346,7 @@ export function AccessGoal() {
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 required>
                 <option value="" disabled>
-                  Select a goal
+                  l
                 </option>
                 {departmentgoal.map((goals) => (
                   <option key={goals._id} value={goals._id}>
@@ -355,6 +354,9 @@ export function AccessGoal() {
                   </option>
                 ))}
               </select>
+              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                Goal
+              </label>
             </div>
             <div className="grid grid-cols-2 gap-2">
               <div className="relative mb-6" data-te-input-wrapper-init>
@@ -671,7 +673,7 @@ export function Userforms(props) {
                     value={role}
                     onChange={(e) => setRole(e.target.value)}
                     required>
-                    <option>Select ...</option>
+                    <option></option>
                     <option value="General Manager">General Manager </option>
                     <option value="Manager">Manager </option>
                     <option value="Junior Staff">Staff </option>
@@ -688,7 +690,6 @@ export function Userforms(props) {
                     className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                     required>
                     <option value="" disabled>
-                      Select a department
                     </option>
                     {departmenttable.map((department) => (
                       <option
