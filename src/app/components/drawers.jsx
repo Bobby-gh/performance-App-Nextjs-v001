9c7e718b19217833b31921fddd2102f834345569
Bobby-gh <knaihv@ymail.com>
2025-01-11 21:22:08 +0000

    adding badges

--- src/app/components/drawers.jsx
+++ src/app/components/drawers.jsx
@@ -174,7 +174,7 @@ export function CreateGoal() {
             <div className="relative mb-6" data-te-input-wrapper-init>
               <select
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                name="workQuality"
+                name="category"
                 value={formData.category}
                 autoComplete="off"
                 onChange={handleInputChange}
