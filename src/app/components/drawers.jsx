509363aa80f8329b024d86fbe78bf01af45d845c
Bobby-gh <knaihv@ymail.com>
2025-01-14 01:35:13 +0000

    changing repo

--- src/app/components/drawers.jsx
+++ src/app/components/drawers.jsx
@@ -28,13 +28,14 @@ export function CreateGoal() {
   const [open, setOpen] = React.useState(false);
   const [isLoading, setLoading] = useState(false);
   const [priority, setPriority] = useState("");
+  const [category, setCategory] = useState("");
+  
   const [formData, setFormData] = useState({
     title: "",
     description: "",
     target: "",
     endDate: "",
     department: "",
-    category: "",
   });
 
   const handleSubmit = async (e) => {
@@ -50,7 +51,7 @@ export function CreateGoal() {
           taskAssignedTo: departments,
           target: formData.target,
           priority: priority,
-          goalType: formData.category,
+          goalType: category,
         }),
         {
           headers: {
@@ -73,13 +74,13 @@ export function CreateGoal() {
   const reload = () => {
     setDepartments("");
     setPriority("");
+    setCategory("");
     setFormData({
       title: "",
       description: "",
       target: "",
       endDate: "",
       department: "",
-      category: "",
     });
   };
 
@@ -176,9 +177,9 @@ export function CreateGoal() {
               <select
                 className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-blue-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                 name="category"
-                value={formData.category}
+                value={category}
                 autoComplete="off"
-                onChange={handleInputChange}
+                onChange={(e) => setCategory(e.target.value)}
                 required>
                 <option></option>
                 <option value="human relationship">Human Relationship</option>
