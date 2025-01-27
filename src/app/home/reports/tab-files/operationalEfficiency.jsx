a1ed625fb628271406c46bbeb44a04085318cdc4
Bobby-gh <knaihv@ymail.com>
2025-01-27 13:53:28 +0000

    route

--- src/app/home/reports/tab-files/operationalEfficiency.jsx
+++ src/app/home/reports/tab-files/operationalEfficiency.jsx
@@ -1,11 +1,11 @@
-import { OperationalEffeciencyTable, TopGoalTable } from "@/app/components/tables";
+import { AccessGoalTable} from "@/app/components/tables";
 import React from "react";
 
 export default function OperationalEfficiency() {
   return (
     <div className="mt-8 mx-8">
       <div className="mt-8 card bg-white rounded-lg">
-        <OperationalEffeciencyTable/>
+        <AccessGoalTable/>
       </div>
     </div>
   );
