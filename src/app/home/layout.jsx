6ad444c7cdb7f26f0a0ea04e0ce9022afabd68c1
Bobby-gh <knaihv@ymail.com>
2025-05-15 10:10:06 +0000

    working on levie forms

--- src/app/home/layout.jsx
+++ src/app/home/layout.jsx
@@ -4,18 +4,16 @@ import { Navbar } from "../components/navbar";
 
 export default function Layout({ children }) {
   return (
-    
-      <main className="flex bg-gray-200 h-screen overflow-hidden">
-        <div className="border border-2 border-r-slate-400 ">
-          <Sidebar />
+    <main className="flex h-screen bg-[#EFF1F9] p-4">
+      <div>
+        <Sidebar />
+      </div>
+      <div className="flex-1 overflow-auto px-4 space-y-4">
+        <div>
+          <Navbar />
         </div>
-        <div className="flex-1 scroll-smooth overflow-auto">
-          <div className="border border-2 border-b-slate-400 ">
-            <Navbar />
-          </div>
-          {children}
-        </div>
-      </main>
-    
+        <div>{children}</div>
+      </div>
+    </main>
   );
 }
