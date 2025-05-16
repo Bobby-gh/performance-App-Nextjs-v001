5d9a9182b9d78ea28809fc2437b85147d3e4bcdd
Bobby-gh <knaihv@ymail.com>
2025-05-16 19:49:03 +0000

    working on testing form

--- src/app/language/language_translation.jsx
+++ src/app/language/language_translation.jsx
@@ -3,7 +3,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 i18n.use(initReactI18next).init({
-  fallbackLng: "en", // Default language
+  fallbackLng: "fr", // Default language
   interpolation: {
     escapeValue: false, // React already escapes values to prevent XSS
   },
