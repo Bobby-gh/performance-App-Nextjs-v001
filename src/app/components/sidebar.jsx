b38910c19784a64ee780c12f0616f0bce51d3932
Bobby-gh <knaihv@ymail.com>
2025-05-16 23:31:58 +0000

    working on testing form

--- src/app/components/sidebar.jsx
+++ src/app/components/sidebar.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useContext } from "react";
 import { MenuItems } from "./menuitems";
 import { usePathname } from "next/navigation";
@@ -7,15 +7,12 @@ import { AuthContext } from "../contex/context-context";
 import { useTranslation } from "react-i18next";
 import Link from "next/link";
 
-
 export function Sidebar() {
   const { t } = useTranslation();
   const currentPathname = usePathname().replace(/\/+$/, "");
-  const {auth} = useContext(AuthContext)
+  const { auth } = useContext(AuthContext);
 
-
-
-   const filteredMenuItems = MenuItems.filter((item) => {
+  const filteredMenuItems = MenuItems.filter((item) => {
     if (auth.refNum === "ref?1!" || auth.role === "ref?1!") {
       return !["/home/goals"].includes(item.path);
     }
@@ -23,7 +20,12 @@ export function Sidebar() {
       return !["/home/department"].includes(item.path);
     }
     if (auth.refNum === "ref?3!") {
-      return !["/home/department", "/home/employees", "/home/goal-setting", "/home/goal-assessment"].includes(item.path);
+      return ![
+        "/home/department",
+        "/home/employees",
+        "/home/goal-setting",
+        "/home/goal-assessment",
+      ].includes(item.path);
     }
     return ![""].includes(item.path);
   });
@@ -48,19 +50,25 @@ export function Sidebar() {
               <Link
                 href={item.path}
                 className="flex flex-col items-center w-full p-1 rounded-lg">
-                {/* Icon with hover and focus effects */}
-                <div
-                  className={classNames(
-                    "text-lg p-2 text-white rounded-lg transition-colors duration-200",
-                    {
-                      "bg-[#08397e] text-white": currentPathname === item.path, // Focused state
-                      "hover:bg-[#08397e] hover:text-white": currentPathname !== item.path, // Hover effect only on the icon
-                    }
-                  )}>
-                  {item.icon}
-                </div>
-                {/* Title (beneath icon, unaffected by focus or hover) */}
-                <span className="text-xs mt-1 text-white ">{t(item.title)}</span>
+                <a>
+                  {/* Icon with hover and focus effects */}
+                  <div
+                    className={classNames(
+                      "text-lg p-2 text-white rounded-lg transition-colors duration-200",
+                      {
+                        "bg-[#08397e] text-white":
+                          currentPathname === item.path, // Focused state
+                        "hover:bg-[#08397e] hover:text-white":
+                          currentPathname !== item.path, // Hover effect only on the icon
+                      }
+                    )}>
+                    {item.icon}
+                  </div>
+                  {/* Title (beneath icon, unaffected by focus or hover) */}
+                  <span className="text-xs mt-1 text-white ">
+                    {t(item.title)}
+                  </span>
+                </a>
               </Link>
             </li>
           ))}
