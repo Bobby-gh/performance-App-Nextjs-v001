87497ac4aef94eebbb38a29621ba48ec45ea3e42
Bobby-gh <knaihv@ymail.com>
2025-05-16 19:51:04 +0000

    working on testing form

--- src/app/components/sidebar.jsx
+++ src/app/components/sidebar.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useContext } from "react";
 import { MenuItems } from "./menuitems";
 import Link from "next/link";
@@ -7,14 +7,13 @@ import classNames from "classnames";
 import { AuthContext } from "../contex/context-context";
 import { useTranslation } from "react-i18next";
 
-
 export function Sidebar() {
   const { t } = useTranslation();
 
-  const currentPathname = usePathname()
-  const {auth} = useContext(AuthContext)
+  const currentPathname = usePathname();
+  const { auth } = useContext(AuthContext);
 
-   const filteredMenuItems = MenuItems.filter((item) => {
+  const filteredMenuItems = MenuItems.filter((item) => {
     if (auth.refNum === "ref?1!" || auth.role === "ref?1!") {
       return !["/home/goals"].includes(item.path);
     }
@@ -22,13 +21,17 @@ export function Sidebar() {
       return !["/home/department"].includes(item.path);
     }
     if (auth.refNum === "ref?3!") {
-      return !["/home/department", "/home/employees", "/home/goal-setting", "/home/goal-assessment"].includes(item.path);
+      return ![
+        "/home/department",
+        "/home/employees",
+        "/home/goal-setting",
+        "/home/goal-assessment",
+      ].includes(item.path);
     }
     return ![""].includes(item.path);
   });
 
   return (
-
     <main className="w-24 h-full bg-[#0b1558]  rounded-lg">
       {/* Logo */}
       <div className="flex justify-center p-2 mb-4">
@@ -43,28 +46,31 @@ export function Sidebar() {
       {/* Menu Items */}
       <div className="flex flex-col items-center text-center">
         <ul className="w-full">
-          {filteredMenuItems.map((item) => (
-            <li key={item.title} className="flex flex-col items-center mb-2">
-              <Link
-                href={item.path}
-                className="flex flex-col items-center w-full p-1 rounded-lg">
-                {/* Icon with hover and focus effects */}
-                <div
-                  className={classNames(
-                    "text-lg p-2 text-white rounded-lg transition-colors duration-200",
-                    {
-                      "bg-[#08397e] text-white": currentPathname === item.path, // Focused state
-                      "hover:bg-[#08397e] hover:text-white": currentPathname !== item.path, // Hover effect only on the icon
-                    }
-                  )}>
-                  {item.icon}
-                </div>
+          {filteredMenuItems.map((item) => {
+            const isActive = currentPathname === item.path;
 
-                {/* Title (beneath icon, unaffected by focus or hover) */}
-                <span className="text-xs mt-1 text-white ">{t(item.title)}</span>
-              </Link>
-            </li>
-          ))}
+            return (
+              <li key={item.title} className="flex flex-col items-center mb-2">
+                <Link
+                  href={item.path}
+                  className="flex flex-col items-center w-full p-1 rounded-lg">
+                  <div
+                    className={classNames(
+                      "text-lg p-2 text-white rounded-lg transition-colors duration-200",
+                      {
+                        "bg-[#08397e] text-white": isActive,
+                        "hover:bg-[#08397e] hover:text-white": !isActive,
+                      }
+                    )}>
+                    {item.icon}
+                  </div>
+                  <span className="text-xs mt-1 text-white">
+                    {t(item.title)}
+                  </span>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </main>
