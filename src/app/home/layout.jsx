f483939c1ab4e9230ad6038fefbff0f7eb6be9d6
Bobby-gh <knaihv@ymail.com>
2025-05-17 00:17:47 +0000

    working on testing form

--- src/app/home/layout.jsx
+++ src/app/home/layout.jsx
@@ -1,22 +1,54 @@
 "use client";
+import { useContext, useEffect, useState } from "react";
 import React from "react";
 import { Sidebar } from "../components/sidebar";
-import { ToastProvider } from "../components/notification";
 import { Navbar } from "../components/navbar";
+import { AuthContext } from "../contex/context-context";
+import { LoadingPage } from "../components/loading";
+import { ToastProvider } from "../components/notification";
+import SystemDown from "../system-down/page";
+import NotAuthorized from "../page-not-authorized/page";
+import Cookies from "js-cookie";
+
 export default function Layout({ children }) {
+  const [isCheckingAuth, setIsCheckingAuth] = useState(true);
+  const [validated, setValidated] = useState(false)
+  const { setAuth } = useContext(AuthContext);
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    if (token) {
+      setAuth((prev) => ({
+        ...prev,
+        token: token,
+      }));
+    }
+    setValidated(true)
+    setIsCheckingAuth(false);
+  }, []);
+
+  if (isCheckingAuth) {
+    return <LoadingPage />;
+  }
 
   return (
-    <main className="flex h-screen bg-[#EFF1F9] p-4">
-      <ToastProvider />
-      <div>
-        <Sidebar />
-      </div>
-      <div className="flex-1 overflow-auto px-4 space-y-4">
-        <div>
-          <Navbar />
-        </div>
-        <div>{children}</div>
-      </div>
-    </main>
+    <div>
+      {validated ? (
+        <main className="flex h-screen bg-[#EFF1F9] p-4">
+          <ToastProvider />
+          <div>
+            <Sidebar />
+          </div>
+          <div className="flex-1 overflow-auto px-4 space-y-4">
+            <div>
+              <Navbar />
+            </div>
+            <div>{children}</div>
+          </div>
+        </main>
+      ) : (
+        <NotAuthorized />
+      )}
+    </div>
   );
 }
