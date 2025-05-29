35d80d6459d81773740c2ebbfb31b1be3cf3c1e4
Bobby-gh <knaihv@ymail.com>
2025-05-29 00:22:22 +0000

    working on detail forms

--- src/app/components/infocards.jsx
+++ src/app/components/infocards.jsx
@@ -759,6 +759,6 @@ export function GoalsHeader() {
 export function OrganizationalEmployees() {
   const { t } = useTranslation();
   return (
-    <div className="text "></div>
+    <div className="tex "></div>
   );
 }
