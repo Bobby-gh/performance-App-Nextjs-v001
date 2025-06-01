5bb1647657534b7f5205439730e7db7ed50413f4
Acheampong Emmanuel <ceajaiyemmanuel@gmail.com>
2025-06-01 00:34:54 +0000

    changing format for goal deadline

--- src/app/components/infocards.jsx
+++ src/app/components/infocards.jsx
@@ -614,7 +614,7 @@ export function GoalDetails() {
         </div>
         <div className="bg-blue-50 p-2 rounded-xl text-center">
           <p className="text-blue-700 text-sm">{t("deadline")}</p>
-          <p className="text-lg font-semibold">{goal.goalDeadline}</p>
+          <p className="text-lg font-semibold">  {new Date(goal.goalDeadline).toISOString().split("T")[0]}</p>
         </div>
       </div>
 
