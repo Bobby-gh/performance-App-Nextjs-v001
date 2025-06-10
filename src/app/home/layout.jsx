8184a05b67f0f2fa59504ebaba8891921b0203ef
Bobby-gh <knaihv@ymail.com>
2025-06-10 20:50:49 +0000

    working with emma

--- src/app/home/layout.jsx
+++ src/app/home/layout.jsx
@@ -39,7 +39,7 @@ export default function Layout({ children }) {
           <div>
             <Sidebar />
           </div>
-          <div className="flex-1 overflow-auto p-4 bg-[#313a67] rounded-lg space-y-4">
+          <div className="flex-1 overflow-auto p-4 bg-white rounded-lg space-y-4">
             <div>
               <Navbar />
             </div>
