0fe2880f4093a8446f583e511e4f2bebd86b4a51
Acheampong Emmanuel <ceajaiyemmanuel@gmail.com>
2025-06-21 22:04:43 +0000

    working on assess columns

--- src/app/components/tables.jsx
+++ src/app/components/tables.jsx
@@ -302,7 +302,7 @@ export function AccessGoalTable() {
   const [open, setOpen] = useState(false);
   const { t } = useTranslation();
   const {auth} = useContext(Modaltrigger);
-  const isManager = auth?.refNum;
+  const isManager = auth?.refNum === "ref?2!";
   const [assessGoalInfo, setAssessGoalInfo] = useState("");
   const { goalAssessment } = useGoalAccessmentRouteData();
   const accessinggoalcolumn = useAccessingGoalColumn();
@@ -334,7 +334,7 @@ export function AccessGoalTable() {
     _id: goal._id,
     goalTitle: goal.goalAssessed?.goalTitle || "",
     goalStatus: goal.goalAssessed?.status || "",
-    taskAssignedTo: goal.goalAssessed?.taskAssignedTo?.departmentName || "",
+    taskAssignedTo: isManager ? goal.goalAssessed?.taskAssignedTo?.fullName : goal.goalAssessed?.taskAssignedTo?.departmentName,
     fullName: goal.goalAssessed?.taskAssignedTo?.fullName || "",
     goalDeadline: new Date(goal.goalAssessed?.goalDeadline).toLocaleDateString() || "",
     performancePercent: goal.averageRating?.performancePercent || 0,
@@ -358,15 +358,7 @@ export function AccessGoalTable() {
  
 
   // Inject MRN column override
-const columns = useMemo(() => {
-  return accessinggoalcolumn.filter((col) => {
-    if (isManager === "ref?2!") {
-      return col.accessorKey !== "taskAssignedTo"; // hide taskAssignedTo for managers
-    } else {
-      return col.accessorKey !== "fullName"; // hide fullname for non-managers
-    }
-  });
-}, [accessinggoalcolumn, isManager]);
+const columns = useMemo(() => {accessinggoalcolumn}, [accessinggoalcolumn]);
 
   const table = useMaterialReactTable({
     columns,
