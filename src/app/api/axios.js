151342946428446862a1938052b181775e9a4828
Acheampong Emmanuel <ceajaiyemmanuel@gmail.com>
2025-10-12 00:05:38 +0000

    working on axios

--- src/app/api/axios.js
+++ src/app/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://performanceappbackendv001-production.up.railway.app/api/v1",
+  //baseURL: "https://performanceappbackendv001-production.up.railway.app/api/v1",
+  baseURL:"performanceappbackendv001-production-7c7a.up.railway.app/api/v1",
 });
