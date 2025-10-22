c9a0383e46ed99f08bc0922d73923f296ad7b1f5
Acheampong Emmanuel <ceajaiyemmanuel@gmail.com>
2025-10-22 12:20:45 +0000

    working

--- src/app/home/reports/tab-files/balanceScoreCard.jsx
+++ src/app/home/reports/tab-files/balanceScoreCard.jsx
@@ -46,7 +46,7 @@ export default function BalanceScoreCard() {
       </button>
       <div ref={chartsRef} >
         <div className="space-y-4">
-        <h1 className="text-6xl font-bold text-gray-900 print:text-5xl">
+        <h1 className="text-6xl font-bold text-gray-900 print:text-5xl items-center mb-8">
           {'General Report'}
         </h1>
         </div>
