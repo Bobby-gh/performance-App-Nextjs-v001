9eb92d404ce52124450d4684d1d09cc2f2954e11
Acheampong Emmanuel <ceajaiyemmanuel@gmail.com>
2026-01-09 18:19:23 +0000

    working on charts

--- src/app/home/reports/tab-files/goalAchievment.jsx
+++ src/app/home/reports/tab-files/goalAchievment.jsx
@@ -1,25 +1,10 @@
 import React from "react";
-import {
-  AchievedGoalChart,
-  NotAchievedChart,
-  PartiallyAchievedChart,
-  TargetAchievementChart,
-} from "@/app/components/charts";
-import { SystemGoalTable } from "@/app/components/tables";
 import FinancialProjections from "@/app/components/widgets";
 
 export default function GoalAchievemnetReport() {
   return (
     <div className="mt-8 mx-8">
-      <div className="grid grid-cols-3 gap-4">
-        <AchievedGoalChart />
-        <PartiallyAchievedChart />
-        <NotAchievedChart />
-      </div>
       <FinancialProjections/> 
-      <div className="mt-8 card bg-white rounded-lg">
-        <SystemGoalTable/>
-      </div>
     </div>
   );
 }
