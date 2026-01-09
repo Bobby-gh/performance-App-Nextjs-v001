1bebe26b8c5275f52d9d4442e7c979fd377ab5b9
Acheampong Emmanuel <ceajaiyemmanuel@gmail.com>
2026-01-09 18:16:35 +0000

    working

--- src/app/components/widgets.jsx
+++ src/app/components/widgets.jsx
@@ -473,8 +473,8 @@ const FinancialProjections = () => {
   ];
 
   return (
-    <div className="min-h-screen">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+    <div className="min-h-screen mt-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
         {cardsData.map((card, index) => (
           <FinancialCard key={index} data={card} />
         ))}
